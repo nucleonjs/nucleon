8151878cc78d717554281e5434b1b4ce0b44a155
Doc077 <dom.rajkowski@gmail.com>
2022-07-14 20:39:19 +0200

    Update API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { createServer } from '@nucleonjs/core';
 import { AppController } from './controllers/app.controller';
+import { createServer } from '@nucleonjs/core';
 
 const server = createServer({
   controllers: [
     AppController,
   ],
+  channels: [],
 });
 
 server.start();
