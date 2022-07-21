d399d954e8867b7951e250d1ac3fa74826091645
Doc077 <dom.rajkowski@gmail.com>
2022-07-21 20:05:30 +0200

    Add db client support

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { AppController } from './controllers/app.controller';
 import { createServer } from '@nucleonjs/core';
+import { PrismaClient as DatabaseClient } from '@prisma/client';
 
-const server = createServer({
+const server = createServer<DatabaseClient>({
+  databaseClient: DatabaseClient,
   controllers: [
     AppController,
   ],
