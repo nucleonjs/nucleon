09a494af920c3d6109aabf1f91a639e0f8761474
Doc077 <dom.rajkowski@gmail.com>
2022-07-24 15:48:32 +0200

    Add constructor

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -2,6 +2,8 @@ import { Controller, render, Route, ViewResponse } from '@nucleonjs/core';
 
 @Controller()
 export class AppController {
+  constructor() {}
+
   @Route.Get('/')
   public index(): ViewResponse {
     return render('pages/home');
